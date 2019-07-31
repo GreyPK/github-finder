6d34daae36896aa5733af3da1a7d01634b935afc
Sergey Dyubov <greypks@gmail.com>
2019-08-01 05:09:42 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+import Users from './components/users/Users';
+import User from './components/users/User';
+import Search from './components/users/Search';
+import About from './components/pages/About';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+	state = {
+		users: [],
+		user: {},
+		loading: false,
+		alert: null
+	}
+
+	async componentDidMount() {
+		this.setState({ loading: true });
+
+		const response = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+		this.setState({ users: response.data, loading: false });
+	}
+
+	searchUsers = async text => {
+		this.setState({ loading: true });
+
+		const response = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+		this.setState({ users: response.data.items, loading: false });
+	}
+
+	getUser = async username => {
+		this.setState({ loading: true });
+
+		const response = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+		this.setState({ user: response.data, loading: false });
+	}
+
+	clearUsers = () => {
+		this.setState({ users: [], loading: false })
+	}
+
+	setAlert = (msg, type) => {
+		this.setState({ alert: { msg, type } });
+		setTimeout(() => {
+			this.setState({ alert: null });
+		}, 5000);
+	}
+
+	render() {
+		const { users, user, loading, alert } = this.state;
+
+		return (
+			<Router>
+				<div className="App">
+					<Navbar />
+					<div className="container">
+						<Alert alert={alert} />
+						<Switch>
+							<Route exact path='/' render={props => (
+								<Fragment>
+									<Search
+										searchUsers={this.searchUsers}
+										clearUsers={this.clearUsers}
+										showClear={users.length ? true : false}
+										setAlert={this.setAlert} />
+									<Users loading={loading} users={users} />
+								</Fragment>
+							)} />
+							<Route exact path='/about' component={About} />
+							<Route exact path='/user/:login' render={props => (
+								<User {...props} getUser={this.getUser} user={user} loading={loading} />
+							)} />
+						</Switch>
+					</div>
+				</div>
+			</Router>
+		);
+	}
+}
+
+export default App;
---
+++ src/components/users/Search.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Search extends Component {
+	state = {
+		text: ''
+	}
+
+	static propTypes = {
+		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.bool.isRequired,
+		setAlert: PropTypes.func.isRequired
+	}
+
+	inputHandler = (e) => {
+		this.setState({ [e.target.name]: e.target.value })
+	}
+
+	onSubmit = (e) => {
+		e.preventDefault();
+		if (!this.state.text) {
+			this.props.setAlert('Please enter something', 'light');
+		} else {
+			this.props.searchUsers(this.state.text);
+			this.setState({ text: '' });
+		}
+	}
+
+	render() {
+		const { showClear, clearUsers } = this.props;
+
+		return (
+			<div>
+				<form onSubmit={this.onSubmit} className="form">
+					<input type="text" name="text" value={this.state.text} placeholder="Search Users..." onChange={this.inputHandler} />
+					<button type="submit" className="btn btn-dark btn-block">Search</button>
+
+					{showClear &&
+						<button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+					}
+				</form>
+			</div>
+		)
+	}
+}
+
+export default Search
